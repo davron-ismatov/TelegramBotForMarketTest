c1f96a80746ccedf7218409c174ae7ef8105389d
Davron <davronismato591@gmail.com>
2024-05-09 18:41:28 +0500

    startin the test project

---
+++ src/main/java/com/example/telegrambotformarkettest/TelegramBotForMarketTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.telegrambotformarkettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForMarketTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TelegramBotForMarketTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telegrambotformarkettest/TelegramBotForMarketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotformarkettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForMarketTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
